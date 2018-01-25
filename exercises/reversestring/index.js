dd3dae56c5063e559c791a63dab2156f5154fc9a
Seungjae Moon <sjaymoon15@gmail.com>
2018-01-25 14:45:29 -0800

    reverse string without using array reverse method

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
 function reverse(str) {
-  return str.split('').reverse().join('');
+  return str.split('').reduce((prev, curr) => curr + prev);
 }
 
 module.exports = reverse;
