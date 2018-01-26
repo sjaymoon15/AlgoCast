3c6fb73392669d7a0f4c62742f6b67216f60301f
Seungjae Moon <sjaymoon15@gmail.com>
2018-01-26 10:54:11 -0800

    add solution to palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  const reversedStr = str.split('').reduce((prev, curr) => curr + prev, '');
+  return str === reversedStr;
+}
 
 module.exports = palindrome;
