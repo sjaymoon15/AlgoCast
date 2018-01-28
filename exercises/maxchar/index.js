55b835fecc9d4e893fcb1ceba059c3f6bf30ca5b
Seungjae Moon <sjaymoon15@gmail.com>
2018-01-28 14:30:57 -0800

    add my first solution to maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let frequency = {};
+  for (let char of str) {
+    if (frequency[char]) {
+      frequency[char]++;
+    } else {
+      frequency[char] = 1;
+    }
+  }
+  let maxValue = 0
+  let maxCharacter = str[0];
+
+  for (let key in frequency) {
+    if (frequency[key] > maxValue) {
+      maxValue = frequency[key];
+      maxCharacter = key;
+    }
+  }
+  return maxCharacter;
+}
 
 module.exports = maxChar;
