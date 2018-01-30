6ff56c6cc25902d8d7429ebec8dcd0a212564a3e
Seungjae Moon <sjaymoon15@gmail.com>
2018-01-30 11:26:27 -0800

    add my first solution to anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const alphabetLetters = 'abcdefghijklmnopqrstuvwxyz';
+  let letterCount = {};
+  for (let elem of stringA) {
+    const elemLowered = elem.toLowerCase();
+    if (alphabetLetters.includes(elemLowered)) {
+      if (letterCount[elemLowered]) {
+        letterCount[elemLowered]++;
+      } else {
+        letterCount[elemLowered] = 1;
+      }
+    }
+  }
+
+  for (let elem of stringB) {
+    const elemLowered = elem.toLowerCase();
+    if (alphabetLetters.includes(elemLowered)) {
+      if (!letterCount[elemLowered]) {
+        return false;
+      } else {
+        letterCount[elemLowered]--;
+      }
+    }
+  }
+
+  for (let letter in letterCount) {
+    if (letterCount[letter] !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = anagrams;
