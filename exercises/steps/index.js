c0338b808865595f2445b15ed22e259bf9f2f7cb
Seungjae Moon <sjaymoon15@gmail.com>
2018-01-31 11:19:25 -0800

    add a solution to steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,25 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  //brute force. nested loops
+  //first loop row
+  //second loop column
+  // for (let r = 0; r <= n; r++)
+  // for (let c = 0; c <= n; c++)
+  // for each row we create a new string to log
+  for (let r = 0; r < n; r++) {
+    let rowStr = '';
+    for (let c = 0; c < n; c++) {
+      if (c <= r) {
+        rowStr += '#';
+      } else {
+        rowStr += ' ';
+      }
+    }
+    //log after looping each row
+    console.log(rowStr);
+  }
+}
 
 module.exports = steps;
