6363c76237db60db24882c7039760a8ff61e0bf4
Seungjae Moon <sjaymoon15@gmail.com>
2018-04-05 22:17:13 -0700

    Add my solution to qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,67 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
+// [1,2,3]
+// []
+// keep stacking in left if right is empty (add)
+// once (peek or remove) are called move them to right until left is empty if right is empty
+// []
+// [3,2,1] -> if peek return right.peek(), if remove return right.pop()
+// let's say added 3 times, and asked for remove Then add(4)
+  // [4]
+  // [3,2,1] -> can keep calling q.remove()
+  // if right is empty, check left and move anything over until left is empty
+
+  // [4,5,6,7]
+  // [3,2,1]
+  //
+
+// add, peek, remove
+
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.left = new Stack();
+    this.right = new Stack();
+  }
+
+  add(record) {
+    this.left.push(record);
+  }
+
+  peek() {
+    if (!this.right.peek()) {
+      if (!this.left.peek()) {
+        return undefined;
+      } else {
+        // move all left stuff to right
+        while (this.left.peek()) {
+          this.right.push(this.left.pop());
+        }
+        return this.right.peek();
+      }
+    } else {
+      return this.right.peek();
+    }
+  }
+
+  remove() {
+    if (!this.right.peek()) {
+      if (!this.left.peek()) {
+        return undefined;
+      } else {
+        // move all left stuff to right
+        while (this.left.peek()) {
+          this.right.push(this.left.pop());
+        }
+        return this.right.pop();
+      }
+    } else {
+      return this.right.pop();
+    }
+  }
+
+}
 
 module.exports = Queue;
